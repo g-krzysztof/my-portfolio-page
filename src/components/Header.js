ee7191c4ad67e476297986310f0a5a6cf5f4247e
Krzysztof Gryc <hello@krzysztofg.pl>
2019-09-06 22:51:28 +0200

    add menu structure and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,14 @@ import '../css/Header.css';
 
 function Header() {
     return (
-        <div className="Header"><h2>Header</h2></div>
+        <div className="Header">
+            <ul className="Header__nav">
+                <li className="Header__item"><span className="Header__link"><i class="fa fa-user-circle-o Header__icon" aria-hidden="true"></i><br />about</span></li>
+                <li className="Header__item"><span className="Header__link"><i class="fa fa-file-text-o Header__icon" aria-hidden="true"></i><br />resume</span></li>
+                <li className="Header__item"><span className="Header__link"><i class="fa fa-desktop Header__icon" aria-hidden="true"></i><br />works</span></li>
+                <li className="Header__item"><span className="Header__link"><i class="fa fa-paper-plane-o Header__icon" aria-hidden="true"></i><br />contact</span></li>
+            </ul>
+        </div>
     );
 }
 
