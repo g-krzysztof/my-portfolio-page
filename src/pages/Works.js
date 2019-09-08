81fc19a79083c3e016cae6c6303d5f0f6479ca0d
Krzysztof Gryc <hello@krzysztofg.pl>
2019-09-08 11:33:21 +0200

    change styles

---
+++ src/pages/Works.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Works() {
+    return (
+        <div className="Content__about">
+            <h2>Works</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sint aperiam, odit eos ad saepe sed impedit fugit. Hic, officiis vero. Dolores consectetur molestias itaque ducimus amet aliquam voluptate et!</p>
+        </div>
+    );
+}
+
+export default Works;
